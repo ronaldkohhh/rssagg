c1020828432ddb0d4aba1ed4cf4070d527626c8a
ronaldkohhh <ronald9872@gmail.com>
2024-04-11 16:16:26 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ronaldkohhh/rssagg
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	fmt.Println("hello  world")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	fmt.Println("Port:", portString)
+}
