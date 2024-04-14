a50320228ad26d957fce18942e522f7aee3ab833
ronaldkohhh <ronald9872@gmail.com>
2024-04-14 16:56:52 +0800

    create feeds

--- sql/schema/003_feeds.sql
+++ sql/schema/003_feeds.sql
@@ -4,9 +4,9 @@ CREATE TABLE feeds (
     id UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    name TEXT NOT NULL
+    name TEXT NOT NULL,
     url TEXT UNIQUE NOT NULL,
-    user_id UUID REFERENCES users(id) ON DELETE CASCADE
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE
 );
 
 -- +goose Down
