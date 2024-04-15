7c148ed1986c7868815f0e00d90f0dcbbff166b9
ronaldkohhh <ronald9872@gmail.com>
2024-04-15 17:36:17 +0800

    update scraper

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -19,6 +21,12 @@ type apiConfig struct {
 }
 
 func main() {
+
+	feed, err := urlToFeed(("https://wagslane.dev/index.xml"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(feed)
 
 	godotenv.Load(".env")
 
@@ -37,9 +45,13 @@ func main() {
 		log.Fatal("Can't connect to databse:", err)
 	}
 
+
+	db := database.New(conn)
 	apiCfg := apiConfig{
-		DB: database.New(conn),
+		DB: db,
 	}
+
+	go startScraping(db, 10, time.Minute)
 
 	router := chi.NewRouter()
 
--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -6,4 +6,14 @@ RETURNING *;
 -- name: GetFeeds :many
 SELECT * FROM feeds;
 
--- name: GetNextFeedToFetch :one
+-- name: GetNextFeedsToFetch :many
+SELECT * FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT $1;
+
+-- name: MarkFeedAsFetched :one
+UPDATE feeds
+SET last_fetched_at = NOW(),
+updated_at = NOW()
+WHERE id = $1
+RETURNING *;
